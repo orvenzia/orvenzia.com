958de61c7954cc49f55a0d081f08ea411208d6a2
orvenzia <orvenzia@gmail.com>
2025-09-04 23:13:11 +0200

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/ (root)
+ ├── index.html
+ ├── about.html
+ ├── services.html
+ ├── ...
+ └── assets
+      ├── css
+      │    └── styles.css
+      ├── js
+      │    └── main.js
+      └── img
+           └── logo.svg
