4506e8ef431f287b38d584e6fe81f8414562e838
orvenzia <orvenzia@gmail.com>
2025-09-10 00:26:09 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,1 @@
-
-document.addEventListener('DOMContentLoaded', function(){
-  const t = document.getElementById('navToggle');
-  const n = document.getElementById('nav');
-  t?.addEventListener('click', ()=> n.classList.toggle('show'));
-});
+document.addEventListener('DOMContentLoaded',()=>{const t=document.getElementById('navToggle');const n=document.getElementById('nav');t?.addEventListener('click',()=>n.classList.toggle('show'));});
