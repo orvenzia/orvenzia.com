3dc9bf004ec8b3792dbfe1018b169167d800c036
orvenzia <orvenzia@gmail.com>
2025-09-15 15:30:04 +0200

    Add files via upload

---
+++ assets/js/menu.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+  const toggle = document.getElementById("menu-toggle");
+  const nav = document.getElementById("nav-links");
+  if(toggle && nav){
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("active");
+    });
+  }
+});
