78c4da47ee0e25d290e2a15479b4533bb30918f3
orvenzia <orvenzia@gmail.com>
2025-10-10 18:00:12 +0200

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener('DOMContentLoaded',()=>{const t=document.getElementById('navToggle');const n=document.getElementById('nav');t?.addEventListener('click',()=>n.classList.toggle('show'));});
